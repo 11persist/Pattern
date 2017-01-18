2f3d83c62b5da51daecf6a3aafdfe4fe5c39f827
11persist <1102422475@qq.com>
2017-01-18 11:09:22 +0800

    add Chain of Responsibility

---
+++ ResponsibilityLink/src/com/imooc/pattern/cor/handler/Sales.java
@@ -0,0 +1,20 @@
+package com.imooc.pattern.cor.handler;
+
+/**
+ * 销售可以批准5%以内的折扣
+ * @author ll110
+ *
+ */
+public class Sales extends PriceHandler{
+
+	@Override
+	public void processDiscount(float discount) {
+		if(discount <= 0.05){
+			System.out.format("%s批准了折扣:%.2f%n",this.getClass().getName(),discount);
+		}else{
+			successor.processDiscount(discount);
+		}
+	}
+	
+	
+}
